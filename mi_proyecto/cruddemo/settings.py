bc283e374054844ad7152278d71c47ae4b5fec45
Agustin186 <agustinguitian16@gmail.com>
2024-06-30 19:05:27 -0300

    completo

--- mi_proyecto/cruddemo/settings.py
+++ mi_proyecto/cruddemo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'cruddemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "cruddemo",
+        "USER": "guitiana",
+        "PASSWORD": "password",
+        "HOST": "db-mysql",
+        "PORT": "3306"
     }
 }
 
